080f8cce4a2b42cc8f6755b31e558497433f91db
Serge Iangicher <sergey.yangicher@rockyroad.io>
2025-02-11 18:33:58 +0200

    exercise 02

---
+++ Exercise02.py
@@ -0,0 +1,31 @@
+import turtle
+import math
+
+def draw_tree(branch_length, level):
+    if level == 0:
+        return
+    
+    turtle.forward(branch_length)
+    turtle.left(45)
+    turtle.color("red")
+    draw_tree(branch_length / math.sqrt(2), level - 1)
+    turtle.right(90)
+    draw_tree(branch_length / math.sqrt(2), level - 1)
+    turtle.left(45)
+    turtle.backward(branch_length)
+
+def main():
+    level = int(input("Enter recursion lvl: "))
+    
+    turtle.speed(0)
+    turtle.left(90)
+    turtle.up()
+    turtle.backward(200)
+    turtle.down()
+    
+    draw_tree(100, level)
+    
+    turtle.done()
+
+if __name__ == "__main__":
+    main()
